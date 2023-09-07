0c10c7e4479e1ca7077fe873903fec1c67d6018e
Crowdbotics <team@crowdbotics.com>
2023-09-07 16:47:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sparkling-lake-43265",
+  url: "https://sparkling-lake-43265.botics.co",
+  api: "https://sparkling-lake-43265.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
